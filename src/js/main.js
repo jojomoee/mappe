7ce7aae2014b2b23d6ef8bf12dbb606d86aafbfb
Johannes Moe <johannesmoehrl@protonmail.com>
2022-05-25 00:32:18 +0200

    basic layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,28 @@
-import './style.css'
+import "../scss/style.scss";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+function init() {
+  const btn = document.getElementsByClassName("list-btn");
+  const overlay = document.getElementById("overlay");
+  const listWrapper = document.getElementById("list-wrapper");
+  const nav = document.querySelector("nav");
+
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", () => {
+      nav.style.backgroundColor = "red";
+      overlay.classList.toggle("overlay_on");
+      overlay.style.display = "flex";
+
+      document.querySelector("body").style.padding = "75px";
+
+      listWrapper.style.display = "none";
+    });
+  }
+
+  nav.addEventListener("click", () => {
+    overlay.classList.toggle("overlay_on");
+    document.querySelector("header").style.display = "";
+    listWrapper.style.display = "";
+  });
+}
+
+document.addEventListener("DOMContentLoaded", init, false);
