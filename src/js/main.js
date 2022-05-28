6b32e16ce2fdb181bae7ac479464fc27a870821d
Johannes Moe <johannesmoehrl@protonmail.com>
2022-05-28 06:34:28 +0200

    i think this code gets cleaner

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,18 @@
 import "../scss/style.scss";
 import gsap from "gsap";
 
+<<<<<<< Updated upstream
+=======
+import {
+  animHeaderNav,
+  stylingNav,
+	listItemAnimation,
+  stylingCloseButton,
+  stylingButton,
+  toggleButton,
+} from "./animations.js";
+
+>>>>>>> Stashed changes
 function init() {
   const btn = document.getElementsByClassName("list-btn");
   const listWrapper = document.getElementById("list-wrapper");
@@ -10,6 +22,7 @@ function init() {
 
   let navState = false;
 
+<<<<<<< Updated upstream
   console.log(navState);
 
   for (let i = 0; i < btn.length; i++) {
@@ -83,6 +96,42 @@ function init() {
       listWrapper.style.pointerEvents = "none";
     }
   });
+=======
+  listItemAnimation(listItem);
+
+//  for (let i = 0; i < listItem.length; i++) {
+//    listItem[i].addEventListener("click", () => {
+//      animListItem(listItem, i, "#list-wrapper");
+//      toggleButton("#btn-right");
+//      toggleButton("#btn-left");
+//
+//      //let styleProp = ["left", "right"];
+//      // stylingButton("btn-right", styleProp[0], styleProp[1]);
+//      navState = true;
+//      window.scrollTo(0, 0);
+//    });
+//  }
+//  //CLOSE LIST-ITEM and TOGGLE CARD
+//  nav.addEventListener("click", () => {
+//    if (navState === true) {
+//      stylingCloseButton(listWrapper, nav);
+//      gsap.from(nav, {
+//        height: 0,
+//      });
+//
+//      for (let i = 0; i < listItem.length; i++) {
+//        gsap.to(listItem[i], {
+//          height: "10vw",
+//          marginBottom: "5vw",
+//          duration: 0.5,
+//        });
+//      }
+//    } else {
+//      nav.style.display = "none";
+//      listWrapper.style.pointerEvents = "none";
+//    }
+//  });
+>>>>>>> Stashed changes
 }
 
 document.addEventListener("DOMContentLoaded", init, false);
