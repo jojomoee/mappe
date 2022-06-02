48b0de953a32c6b10e357b368f9a07f736bbbb9e
Johannes Moe <johannesmoehrl@protonmail.com>
2022-06-02 09:00:36 +0200

    layout onclick

--- src/js/components/item/animation.item.js
+++ src/js/components/item/animation.item.js
@@ -1,28 +1,41 @@
 import gsap from "gsap";
 
-
 export function expandLiOnClick(collection) {
   let itemArr = [];
   for (let item of collection) {
     itemArr.push(item);
     item.addEventListener("click", () => {
+
+      item.children[0].children[2].style.visibility = "visible";
       let tl = gsap.timeline();
       tl.to(item, {
         height: "90vh",
         marginTop: 0,
       });
       let selectedItem = itemArr.indexOf(item);
-
-      for (let i = 0; i < item.children.length; i++) {
-      }
       itemArr.splice(selectedItem, 1);
       for (let unselectedItems of itemArr) {
         let tl = gsap.timeline();
         tl.to(unselectedItems, {
           height: "2vh",
           marginTop: 0,
+          pointerEvents: "none",
         });
       }
     });
+  }
+}
+
+export function hoverEffect(collection) {
+  for (let item of collection) {
+    item.addEventListener("mouseover", () => {
+      item.children[0].children[0].style.display = "none";
+      item.children[0].children[1].style.visibility = "visible";
+    });
+
+    item.addEventListener("mouseleave", () => {
+      item.children[0].children[0].style.display = "block";
+      item.children[0].children[1].style.visibility = "hidden";
+	});
   }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,8 @@ import {
   h1MovingAnimation,
   openingAnimation,
 } from "./components/list/animation.list";
-import { expandLiOnClick } from "./components/item/animation.item";
+import { expandLiOnClick, hoverEffect } from "./components/item/animation.item";
+
 
 function init() {
   let liColl = document.getElementsByClassName("project-li");
@@ -10,6 +11,7 @@ function init() {
 
   let containerColl = document.getElementsByClassName("project-header");
   expandLiOnClick(liColl);
+  hoverEffect(liColl);
   h1MovingAnimation(containerColl);
 }
 
